15f5d738e5b0e09326b7074b3534e258f9604a24
Jungho Kang <jkangpy@gmail.com>
2022-01-07 14:45:58 +0000

    Revision6-Removing unnecessary comments

--- include/process.h
+++ include/process.h
@@ -11,15 +11,14 @@ class Process {
   // Constructor function
   Process(int pid):pid_(pid) {}
   
-  int Pid();                               // (TODO: )See src/process.cpp
-  std::string User();                      // (TODO: )See src/process.cpp
-  std::string Command();                   // (TODO: )See src/process.cpp
-  float CpuUtilization() const;            // (TODO: )See src/process.cpp
-  std::string Ram() const;                 // (TODO: )See src/process.cpp
-  long int UpTime();                       // (TODO: )See src/process.cpp
-  bool operator<(Process const& a) const;  // (TODO: )See src/process.cpp
+  int Pid();                               
+  std::string User();                      
+  std::string Command();                   
+  float CpuUtilization() const;            
+  std::string Ram() const;                 
+  long int UpTime();                       
+  bool operator<(Process const& a) const;  
 
-  // (TODO: )Declare any necessary private members
  private:
   int pid_;
   std::string user_;
